4469f939ee5fa13250e08dcb81bbd778a85eda60
Kellen <142949567+Hurlicane24@users.noreply.github.com>
2024-12-06 01:35:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import sys
 import numpy as np
 import pandas as pd
 
-np.set_printoptions(threshold=sys.maxsize)
-
 storage_space = int(sys.orig_argv[2])
 block_size = int(sys.orig_argv[3])
 if storage_space % block_size == 0: block_num = storage_space // block_size
@@ -71,10 +69,9 @@ def main():
                         free_block_num -= 1
                         blocks_left -= 1
                         file_block += 1
-                    if disk_block < len(block_arr) - 1: # Necessary?
+                    if disk_block < len(block_arr) - 1:
                         disk_block += 1
                     else:
-                        #print("ERROR: block index exceeds total number of blocks.")
                         break
             else:
                 # JOB TOO LARGE
@@ -180,30 +177,29 @@ def remove(file_id):
     print(f"\nFile {file_id} successfully removed.\n")
 
 def printState():
+    print()
+    columns = []
+    for i in range(block_num):
+        columns.append(f"File Block {i + 1}")
+    block_table = pd.DataFrame(columns=columns)
 
-    '''page_table_df = pd.DataFrame.from_dict(fat, orient="index")
-    page_table_df.fillna(-100000, inplace=True)
-    page_table_df = page_table_df.astype(int)
-    page_table_df = page_table_df.applymap(lambda x: "| block" + str(x).rjust(3) if type(x) == int else "Suspended")
-    page_table_df.index.name = "File ID"
-    page_table_df.columns = [f"Block {i}" for i in range(page_table_df.shape[1])]
-    #page_table_df["   Internal Fragmentation"] = int_frag
+    for file_ID in file_ids:
+        disk_blocks = list(fat[file_ID].values())
+        for i in range(len(disk_blocks)):
+            disk_blocks[i] = f"Disk Block {disk_blocks[i]}"
+        length = len(disk_blocks)
+        if(length < block_num):
+            diff = block_num - length
+            for i in range(diff):
+                disk_blocks.append("None")
+        block_table.loc[f"File {file_ID}"] = disk_blocks
 
-    frame_df = pd.DataFrame(block_arr[:, 0], columns=["File ID"])
-    frame_df.index.name = "Block Number"
-    frame_df = frame_df.iloc[0:]
+    print(block_table)
+    print()
 
-    #print("\n\nPAGING SYSTEM SIMULATOR:\n\nThis system emulates a simple file system. Printed below are the file_block tables for each process (job) in the system, as well as the internal fragmentation for each job. Free frames are denoted as 'FREE'. Note that a negative disk_block value indicates a disk_block in secondary storage. Below that is a table showing each disk_block in main memory and the ID of the job occupying that disk_block. Free frames are also denoted as 'FREE'.")
-
-    print("\nFile Allocation Tables:\n")
-    print(page_table_df.replace(str("| disk_block-100000"), "|     FREE").to_string())
-
-    print("\n\nStorage:\n")
-    print(frame_df.replace(0, "FREE").to_string())
-    print("\n")'''
-
-    for key, value in fat.items():
-        print(f"Key: {key}   Value: {value}")
-    print(block_arr)
+    print("Disk Block".ljust(20) + "File ID".ljust(20))
+    for i in range(len(block_arr)):
+        print(f"{i+1}".ljust(20) + str(block_arr[i, 0]))
+    print()
 
 main()
